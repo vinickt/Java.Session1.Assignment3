b2f7cf0fd1f8cc5dc2619aa3b057376274c0b050
vinickt <vini.ckt@gmail.com>
2017-01-25 19:07:48 +0530

    Add files via upload

---
+++ SwapWithoutThirdVariable.java
@@ -0,0 +1,26 @@
+
+public class SwapWithoutThirdVariable {
+	
+	public static void main(String s[])
+	{
+		int num1 = 10;
+		int num2 = 20;
+		
+		System.out.println("Before Swapping");
+		System.out.println();
+		System.out.println("Value of num1 is "+num1);
+		System.out.println("Value of num2 is "+num2);
+		System.out.println();
+		
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
+		
+		System.out.println("After Swapping");
+		System.out.println();
+		System.out.println("Value of num1 is "+num1);
+		System.out.println("Value of num2 is "+num2);
+		
+	}
+
+}
